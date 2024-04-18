911a3c4188f13c6f4bb9b5af4358f173e584c62c
nd2204 <dn200422@gmail.com>
2024-04-18 22:35:10 +0700

    REFACTOR: added function to query private default value table

--- lua/quickimpl/config.lua
+++ lua/quickimpl/config.lua
@@ -1,4 +1,9 @@
-local config = {}
+local config = { }
+
+local default = {
+  add_header_include = true,
+  brace_pattern = " {\n\t\n}\n\n"
+}
 
 local function first_non_nil(...)
   local n = select('#', ...)
@@ -10,16 +15,18 @@ local function first_non_nil(...)
   end
 end
 
-local default = {
-  add_header_include = true,
-}
-
-function config.setup(options)
-  if options == nil then
-    options = {}
+function config.setup(opts)
+  for k, _ in pairs(default) do
+    if opts[k] ~= nil then
+      default[k] = opts[k] 
+    end
   end
 
-  vim.g.generate_add_header_include = first_non_nil(options.add_header_include, default.add_header_include)
+  vim.g.generate_add_header_include = first_non_nil(opts.add_header_include, opts.add_header_include)
+end
+
+function config.getDefaultValue(key)
+  return default[key]
 end
 
 return config
